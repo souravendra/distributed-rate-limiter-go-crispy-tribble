8584469db9558ef5b37476094f97f3d452db730c
Souravendra Krishna Deb <souravendra.official@gmail.com>
2025-04-01 16:54:47 +0530

    feat: added basic hello world endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/souravendra/distributed-rate-limiter-go-crispy-tribble
 
 go 1.24.1
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"encoding/json"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)    // Logs all requests
+	r.Use(middleware.Recoverer) // Graceful panic recovery
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to my Go API!"))
+	})
+
+	r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+		// w.Write([]byte(`{"message": "Hello from Go!"}`))
+		json.NewEncoder(w).Encode(map[string]string{"message": "Hello from Go!"})
+	})
+
+	http.ListenAndServe(":8080", r)
 }
